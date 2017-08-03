fbdd910ebb097d4ee255d08695c50a98a0c06fdb
MattMenna <mattmenna@gmail.com>
2017-08-03 11:34:10 -0400

    Fixed web.xml

--- src/main/java/com/company/controller/HomeController.java
+++ src/main/java/com/company/controller/HomeController.java
@@ -1,23 +1,19 @@
 package com.company.controller;
 
 import org.apache.http.HttpHost;
+import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.HttpClientBuilder;
-import org.hibernate.Criteria;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
-import org.hibernate.cfg.Configuration;
-import org.hibernate.criterion.Restrictions;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.util.ArrayList;
+import java.io.IOException;
 
 @Controller
 public class HomeController {
@@ -26,17 +22,41 @@ public class HomeController {
 
     public ModelAndView helloWorld() // gets a webpage using the ModelandView method
     {
-        //httpclient interface represents the contract for the hteep request excuection
-        HttpClient http = HttpClientBuilder.create().build();
-        // htttphost holds the variables needed for connection
-        // http port default http 80 https 443
 
-        HttpHost host = new HttpHost("forecast.weather.gov", 80, "http");
-        //HttpGet retreives the infor identified by the request uri (in form of an entity)
-        HttpGet getPage = new HttpGet("MapClick.php?lat=42.331427&lon=-83.045754&FcstType=json");
+        String prodCenter = ""; // declared outside for scope issues
+        try {
+            //httpclient interface represents the contract for the hteep request excuection
+            HttpClient http = HttpClientBuilder.create().build();
+            // htttphost holds the variables needed for connection
+            // http port default http 80 https 443
+
+            HttpHost host = new HttpHost("forecast.weather.gov", 80, "http");
+            //HttpGet retreives the infor identified by the request uri (in form of an entity)
+            HttpGet getPage = new HttpGet("MapClick.php?lat=42.331427&lon=-83.045754&FcstType=json");
+
+
+            HttpResponse resp = http.execute(host, getPage);
+
+            String jsonString = EntityUtils.toString(resp.getEntity());
+
+            // assign the returned result to a json object
+
+            JSONObject json = new JSONObject(jsonString);
+
+            prodCenter = json.get("productionCenter").toString();
+
+            //Response code prints to console
+            System.out.println("Response code: " + resp.getStatusLine().getStatusCode());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
 
         return new // useful for adding one or two things to a page the model is the data
-                ModelAndView("welcome", "message", "Hello World");
+                ModelAndView("welcome", "message", prodCenter);
     }
 
 
